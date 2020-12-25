8fb592be8f56f9fd23a38518164b505ee5e8af33
Talisha White <misstalishawhite@gmail.com>
2020-12-25 18:06:27 +0000

    adding some content

--- pages/js/scripts.js
+++ pages/js/scripts.js
@@ -23,3 +23,13 @@ window.addEventListener('load', function(){
 			document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
 			document.getElementById('txtFormatUrl').value = '';
 		}
+
+    //js for topic scroll bar
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
